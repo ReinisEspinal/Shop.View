a7ece6bee8227661b27948d53fc83080633eb793
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-05-20 01:46:25 -0400

    Agrega un odulo nurvo de NAV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import {OrdersModule} from './orders/orders.module';
+import { ListComponent } from "./orders/list/list.component";
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavMenuComponent,
+    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule
+    OrdersModule,
+    HttpClientModule,
+    RouterModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'orders', component: ListComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{ path: 'orders', redirectTo: 'orders/index', pathMatch: 'full' },
+{ path: 'orders/list', component: ListComponent },
+{ path: 'orders/:playerId/details', component: DetailsComponent },
+{ path: 'orders/create', component: CreateComponent },
+{ path: 'orders/:playerId/edit', component: EditComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
